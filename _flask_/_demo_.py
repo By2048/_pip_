56debff8aabdfc79211eb7ed24c6796b439cd3ad
By2048 <1247079575@qq.com>
2022-02-01 10:13:22 +0800

    update

--- _flask_/_demo_.py
+++ _flask_/_demo_.py
@@ -14,3 +14,6 @@ def index():
 if __name__ == '__main__':
     print(f'server start at {PORT}')
     app.run(host='0.0.0.0', port=PORT, debug=True)
+# set FLASK_APP=hello
+# set FLASK_ENV=development
+# flask run
